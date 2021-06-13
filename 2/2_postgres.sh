28492e1d4efa702cac3a6bedc539788b5d14dbcc
esentemov <root@ansible.local>
2021-06-13 13:16:25 +0500

    2+3 task

---
+++ 2/2_postgres.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+echo "This script will helps you to configure postgres"
+
+# Check for root or sudo
+if [[ $(id -u) -ne 0 ]]
+then
+echo " --- Execute with root or sudo --- "
+exit 1
+fi
+
+# Read PGDATA path
+echo "Choose root directory for postgres (default path is /data/postgres/12/data/): "
+read PGDATA
+
+if [ $PGDATA="" ]
+then
+PGDATA="/data/postgres/12/data/"
+fi
+
+echo PGDATA=$PGDATA
+
+# Make directory for PGDATA
+mkdir -p $PGDATA
+chown postgres:postgres $PGDATA
+chmod -R go-rwx $PGDATA
+
+# Change .bash_profile of postgres user
+echo "[ -f /etc/profile ] && source /etc/profile" > '/var/lib/pgsql/.bash_profile'
+echo "PGDATA=${PGDATA}" >> '/var/lib/pgsql/.bash_profile'
+echo "export PGDATA" >> '/var/lib/pgsql/.bash_profile'
+
+# INIT DB
+#su -c "${INITDB} --pwprompt -D ${PGDATA}" postgres
+
+# Allowed subnet to connect from
+echo "Enter allowed subnet to access DB (default it 192.168.0.0/24): "
+read allowed_subnet
+
+if [[ $allowed_subnet = "" ]]
+then
+allowed_subnet='192.168.0.0/24'
+fi
+
+# Add allowed subnet on configuration
+echo "host all all ${allowed_subnet} md5" >> $PGDATA'pg_hba.conf'
