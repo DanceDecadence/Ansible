4824509250b492f3825cf1718fb221175f7bf7b3
esentemov <eug.sent@gmail.com>
2021-06-15 18:50:33 +0500

    This fkn 2 and 3 tasks finally works

--- 2_postgres.sh
+++ 2_postgres.sh
@@ -1,48 +1,76 @@
 #!/bin/bash
 
-echo "This script will helps you to configure postgres"
+### Check for root or sudo ###
 
-# Check for root or sudo
-#if [[ $(id -u) -ne 0 ]]
-#then
-#echo " --- Execute with root or sudo --- "
-#exit 1
-#fi
-
-# Read PGDATA path
-echo "Choose root directory for postgres (default path is /data/postgres/12/data/): "
-read PGDATA
-
-if [ $PGDATA="" ]
+if [ $(id -u) != 0 ]
 then
-export PGDATA=/data/postgres/12/data
+echo " --- Execute with root or sudo --- "
+exit 1
 fi
 
-echo PGDATA=$PGDATA
 
-# Make directory for PGDATA
+### Search for initdb ###
+
+INITDB=$(find / -name initdb)
+
+if [ "$INITDB" = "" ]
+then
+echo "Install postgresql first"
+exit 2
+fi
+
+echo "initdb location is: $INITDB"
+
+
+### Read PGDATA path ###
+
+echo "Choose root directory for postgres [default path is /data/postgres/12/data]: "
+read PGDATA
+
+if [ "$PGDATA" = "" ]
+then
+PGDATA=/data/postgres/12/data/
+fi
+
+
+### Create PGDATA folder ###
+
 mkdir -p $PGDATA
-chown postgres:postgres $PGDATA
-chmod -R go-rwx $PGDATA
+chown -R postgres:postgres $PGDATA
+chmod -R 0750 $PGDATA
 
-# Change .bash_profile of postgres user
-echo "[ -f /etc/profile ] && source /etc/profile" > '/var/lib/pgsql/.bash_profile'
-echo "PGDATA=${PGDATA}" >> '/var/lib/pgsql/.bash_profile'
-echo "export PGDATA" >> '/var/lib/pgsql/.bash_profile'
-
-# INIT DB
-#su -c "${INITDB} --pwprompt -D ${PGDATA}" postgres
-su -c /usr/pgsql-12/bin/initdb postgres
+echo "pgdata location is: $PGDATA"
 
 
-# Allowed subnet to connect from
-#echo "Enter allowed subnet to access DB (default it 192.168.0.0/24): "
+### Change PGDATA in bash profile of postgres user ###
+
+echo "[ -f /etc/profile ] && source /etc/profile" > /var/lib/pgsql/.bash_profile
+echo "PGDATA=$PGDATA" >> /var/lib/pgsql/.bash_profile
+echo "export PGDATA" >> /var/lib/pgsql/.bash_profile
+
+
+### initialize DB cluster ###
+
+/usr/pgsql-12/bin/postgresql-12-setup initdb
+systemctl enable postgresql-12
+systemctl start
+su - postgres -c "$INITDB -D $PGDATA"
+su - postgres -c "/usr/pgsql-12/bin/pg_ctl -D /data/postgres/12/data -l logfile start"
+pkill -u postgres
+#systemctl enable postgresql-12
+#systemctl start postgresql-12
+
+
+### Read allowed subnet ###
+
+#echo "Enter allowed subnet to access DB [192.168.0.0/24]: "
 #read allowed_subnet
 
-#if [[ $allowed_subnet = "" ]]
-#then
+#if [ "$allowed_subnet" = "" ] 
+#then 
 #allowed_subnet='192.168.0.0/24'
 #fi
 
-# Add allowed subnet on configuration
-#echo "host all all ${allowed_subnet} md5" >> $PGDATA'pg_hba.conf'
+
+# Add subnet to pg_hba.conf
+#echo "host all all ${allowed_subnet} md5" >> $(find $PGDATA -name pg_hba.conf)
